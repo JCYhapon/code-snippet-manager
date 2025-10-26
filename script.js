615e7e05846d3e50ccad640a313aeec11ef97c3c
John Carlo Yhapon <yhaponjc@gmail.com>
2025-10-26 22:10:45 +0800

    adding all the elements from html

---
+++ script.js
@@ -0,0 +1,8 @@
+const numberOfSnippets = document.querySelector('#snippets');
+const numberOfLanguages = document.querySelector('#languages');
+const numberOfTags = document.querySelector('#tags');
+const addSnippetsEl = document.querySelector('#addSnippet');
+const searchSnippetsEl = document.querySelector('#searchSnippets').value;
+const listTagsEl = document.querySelector('#listTags');
+const listSnippetsEl = document.querySelector('#listSnippets');
+
