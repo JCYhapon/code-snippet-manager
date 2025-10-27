a418bf77cee8b20ff23470a3db6b9e2fc17df952
John Carlo Yhapon <yhaponjc@gmail.com>
2025-10-27 21:19:41 +0800

    removed the value on searchSnippets

--- script.js
+++ script.js
@@ -2,7 +2,17 @@ const numberOfSnippets = document.querySelector('#snippets');
 const numberOfLanguages = document.querySelector('#languages');
 const numberOfTags = document.querySelector('#tags');
 const addSnippetsEl = document.querySelector('#addSnippet');
-const searchSnippetsEl = document.querySelector('#searchSnippets').value;
+const searchSnippetsEl = document.querySelector('#searchSnippets');
 const listTagsEl = document.querySelector('#listTags');
 const listSnippetsEl = document.querySelector('#listSnippets');
+const addSnippetModalEl = document.querySelector('#addSnippetModal');
+const snippetTitleEl = document.querySelector('#snippetTitle');
+const snippetCodeEl = document.querySelector('#snippetCode');
+const snippetLanguageEl = document.querySelector('#snippetLanguage');
+const snippetTagsEl = document.querySelector('#snippetTags');
+const snippetDescriptionEl = document.querySelector('#snippetDescription');
+const snippetFormEl = document.querySelector('#snippetForm'); 
 
+addSnippetsEl.addEventListener('click', () => {
+    addSnippetModalEl.classList.remove('hidden');
+})
