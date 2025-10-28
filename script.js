c2ef2c959c2564e8bcccfb49ce51eac4f3371493
John Carlo Yhapon <yhaponjc@gmail.com>
2025-10-28 21:38:55 +0800

    create addSnippet for the submit eventlistener and provide the value on the inputs of modal

--- script.js
+++ script.js
@@ -1,18 +1,41 @@
-const numberOfSnippets = document.querySelector('#snippets');
-const numberOfLanguages = document.querySelector('#languages');
-const numberOfTags = document.querySelector('#tags');
-const addSnippetsEl = document.querySelector('#addSnippet');
-const searchSnippetsEl = document.querySelector('#searchSnippets');
-const listTagsEl = document.querySelector('#listTags');
-const listSnippetsEl = document.querySelector('#listSnippets');
-const addSnippetModalEl = document.querySelector('#addSnippetModal');
-const snippetTitleEl = document.querySelector('#snippetTitle');
-const snippetCodeEl = document.querySelector('#snippetCode');
-const snippetLanguageEl = document.querySelector('#snippetLanguage');
-const snippetTagsEl = document.querySelector('#snippetTags');
-const snippetDescriptionEl = document.querySelector('#snippetDescription');
-const snippetFormEl = document.querySelector('#snippetForm'); 
+const numberOfSnippets = document.querySelector("#snippets");
+const numberOfLanguages = document.querySelector("#languages");
+const numberOfTags = document.querySelector("#tags");
+const addSnippetsEl = document.querySelector("#addSnippet");
+const searchSnippetsEl = document.querySelector("#searchSnippets");
+const listTagsEl = document.querySelector("#listTags");
+const listSnippetsEl = document.querySelector("#listSnippets");
+const addSnippetModalEl = document.querySelector("#addSnippetModal");
+const snippetFormEl = document.querySelector("#snippetForm");
+const closeModalEl = document.querySelector(".close");
 
-addSnippetsEl.addEventListener('click', () => {
-    addSnippetModalEl.classList.remove('hidden');
-})
+addSnippetsEl.addEventListener("click", () => {
+  addSnippetModalEl.classList.remove("hidden");
+});
+
+closeModalEl.addEventListener("click", () => {
+  addSnippetModalEl.classList.add("hidden"); // hide the modal again
+  document.body.style.overflow = ""; // restore scrolling
+});
+
+snippetFormEl.addEventListener("submit", addSnippet);
+
+function addSnippet(e) {
+  //Prevent the page to reload once the submit button was clicked
+  e.preventDefault();
+  const title = document.querySelector("#snippetTitle").value;
+  const code = document.querySelector("#snippetCode").value;
+  const language = document.querySelector("#snippetLanguage").value;
+  const tag = document.querySelector("#snippetTags").value;
+  const description = document.querySelector("#snippetDescription").value;
+
+  const snippet = {
+    title,
+    code,
+    language,
+    tag,
+    description
+  }
+
+  console.log(snippet);
+}
